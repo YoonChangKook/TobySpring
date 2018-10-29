16faf48ed466e8138cce0e086a592b531060a362
YoonchangKook <yoonchang.kook@nts-corp.com>
2018-10-29 10:45:09 +0900

    UserService add 및 upgrade level 추가

--- src/main/java/com/tobi/junit/UserServiceTest.java
+++ src/main/java/com/tobi/junit/UserServiceTest.java
@@ -4,22 +4,84 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.tobi.user.dao.UserDao;
+import com.tobi.user.dto.Level;
+import com.tobi.user.dto.User;
 import com.tobi.user.service.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class UserServiceTest {
 	@Autowired
-	UserService userService;
+	private UserService userService;
+
+	@Autowired
+	private UserDao userDao;
+
+	private List<User> users;
+
+	@Before
+	public void setUp() {
+		this.users = Arrays.asList(
+			new User("yoonchang", "국윤창", "p1", Level.BASIC, 49, 0),
+			new User("hangyul", "김한결", "p2", Level.BASIC, 50, 0),
+			new User("sunghyun", "김성현", "p3", Level.SILVER, 60, 29),
+			new User("minsik", "황민식", "p4", Level.SILVER, 60, 30),
+			new User("hyuknae", "권혁내", "p5", Level.GOLD, 100, 100)
+		);
+	}
 
 	@Test
 	public void bean() {
 		assertThat(this.userService, is(notNullValue()));
+	}
+
+	@Test
+	public void upgradeLevels() {
+		userDao.deleteAll();
+		for(User user : users) {
+			userDao.add(user);
+		}
+
+		userService.upgradeLevels();
+
+		checkLevel(users.get(0), Level.BASIC);
+		checkLevel(users.get(1), Level.SILVER);
+		checkLevel(users.get(2), Level.SILVER);
+		checkLevel(users.get(3), Level.GOLD);
+		checkLevel(users.get(4), Level.GOLD);
+	}
+
+	private void checkLevel(User user, Level expectedLevel) {
+		User userUpdate = userDao.get(user.getId());
+		assertThat(userUpdate.getLevel(), is(expectedLevel));
+	}
+
+	@Test
+	public void add() {
+		userDao.deleteAll();
+
+		User userWithLevel = users.get(4);
+		User userWithoutLevel = users.get(0);
+		userWithoutLevel.setLevel(null);
+
+		userService.add(userWithLevel);
+		userService.add(userWithoutLevel);
+
+		User userWithLevelRead = userDao.get(userWithLevel.getId());
+		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
 	}
 }
--- src/main/java/com/tobi/user/service/UserService.java
+++ src/main/java/com/tobi/user/service/UserService.java
@@ -1,11 +1,56 @@
 package com.tobi.user.service;
 
+import java.util.List;
+
 import com.tobi.user.dao.UserDao;
+import com.tobi.user.dto.Level;
+import com.tobi.user.dto.User;
 
 public class UserService {
 	UserDao userDao;
 
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
+	}
+
+	public void upgradeLevels() {
+		List<User> users = userDao.getAll();
+		for (User user : users) {
+			if (canUpgradeLevel(user)) {
+				upgradeLevel(user);
+			}
+		}
+	}
+
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel) {
+			case BASIC:
+				return (user.getLogin() >= 50);
+			case SILVER:
+				return (user.getRecommend() >= 30);
+			case GOLD:
+				return false;
+			default:
+				throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+		}
+	}
+
+	private void upgradeLevel(User user) {
+		if (user.getLevel() == Level.BASIC) {
+			user.setLevel(Level.SILVER);
+		} else if (user.getLevel() == Level.SILVER) {
+			user.setLevel(Level.GOLD);
+		}
+
+		userDao.update(user);
+	}
+
+	public void add(User user) {
+		if (user.getLevel() == null) {
+			user.setLevel(Level.BASIC);
+		}
+
+		userDao.add(user);
 	}
 }
