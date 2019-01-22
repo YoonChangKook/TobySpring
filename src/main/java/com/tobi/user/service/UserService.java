2225ec4bfc007a536afe949c2cc5624f5687d7c4
YoonchangKook <yoonchang.kook@nts-corp.com>
2019-01-22 20:51:23 +0900

    애노테이션 방식으로 트랜잭션 적용
     - 애노테이션 방식의 프록시 자동 적용 방법으로 트랜잭션을 적용하도록 변경함

--- src/main/java/com/tobi/user/service/UserService.java
+++ src/main/java/com/tobi/user/service/UserService.java
@@ -2,9 +2,13 @@ package com.tobi.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.tobi.user.dto.User;
 
+@Transactional
 public interface UserService {
+	@Transactional(readOnly = true)
 	List<User> getAll();
 	void add(User user);
 	void upgradeLevels();
